a32c84b2b7d34b4f1e290ef0ac9a8eb4da135ad6
Nikita Haidai <90444574+fp0x@users.noreply.github.com>
2021-10-11 22:22:23 +0300

    Add files via upload

--- data-service.py
+++ data-service.py
@@ -3,50 +3,50 @@ from dataclasses import dataclass
 @dataclass
 class TypeOfMainAssets:
     code: int
-    type: str
+    name: str
 
 @dataclass
 class MoveOfMainAssets:
     name: str
+    data: float
     code: int
     remainder: float
     received: float
     output: float
 
 type_array = []
-type_array.append(TypeOfMainAssets(1, "Будівлі"))
-type_array.append(TypeOfMainAssets(2, "Машини та обладнання"))
-type_array.append(TypeOfMainAssets(3, "Транспортні засоби"))
-type_array.append(TypeOfMainAssets(4, "Інструмент та інвентар"))
-
+type_array.append(TypeOfMainAssets(100, "Бесарабський ринок"))
+type_array.append(TypeOfMainAssets(200, "Житній ринок"))
+type_array.append(TypeOfMainAssets(300, "Володимирський ринок"))
 move_array = []
-move_array.append(MoveOfMainAssets("Універсам 22", 1, 44048.00, 500.00, 200.00))
-move_array.append(MoveOfMainAssets("Дніпрянка", 1, 22456.00, 365.00, 123.00))
-move_array.append(MoveOfMainAssets("Універсам 22", 2, 5564.00, 2571.00, 135.00))
-move_array.append(MoveOfMainAssets("Дніпрянка", 2, 10087.00, 15972.00, 58.00))
-move_array.append(MoveOfMainAssets("Універсам 22", 3, 0.00, 0.00, 0.00))
-move_array.append(MoveOfMainAssets("Дніпрянка", 3, 206.00, 34.00, 50.00))
-move_array.append(MoveOfMainAssets("Універсам 22", 4, 116.00, 64.00, 23.00))
-move_array.append(MoveOfMainAssets("Дніпрянка", 4, 34.00, 23.00, 25.00))
+move_array.append(MoveOfMainAssets("Бесарабський ринок", "02.11.2016", 100, 45.00, 50.00, 70.00))
+move_array.append(MoveOfMainAssets("Житній ринок", "02.11.2016", 200, 35.00, 30.00, 50.00))
+move_array.append(MoveOfMainAssets("Володимирський ринок", "02.11.2016", 300, 35.00, 30.00, 45.00))
+move_array.append(MoveOfMainAssets("Бесарабський ринок", "14.11.2016", 100, 45.00, 45.00, 60.00))
+move_array.append(MoveOfMainAssets("Житній ринок", "14.11.2016", 200, 40.00, 40.00, 50.00))
+move_array.append(MoveOfMainAssets("Володимирський ринок", "14.11.2016", 300, 35.00, 35.00, 45.00))
+move_array.append(MoveOfMainAssets("Бесарабський ринок", "22.11.2016", 100, 40.00, 40.00, 60.00))
+move_array.append(MoveOfMainAssets("Житній ринок", "22.11.2016", 200, 40.00, 40.00, 50.00))
+move_array.append(MoveOfMainAssets("Володимирський ринок", "22.11.2016", 300, 40.00, 40.00, 60.00))
 
 def printMoveOfMainAssets(moveOfMainAssets):
     '''printMoveOfMainAssets function prints
-    "Рух основних засобів"
+    "Статистичні дані про ринкові ціни"
     with names and values'''
 
-    print("Назва: {name}, Код: {code}, Залишок,грн.: {remainder}, Надійшло,грн.: {received} Вибуток, грн: {output}"
+    print ("Код: {code}, Найменування ринку: {name}, Дата: {data}, Картопля,грн.: {remainder}, Капуста,грн.: {received} Цибуля, грн: {output}"
           .format(name=moveOfMainAssets.name, code=moveOfMainAssets.code, remainder=moveOfMainAssets.remainder,
-                  received=moveOfMainAssets.received, output=moveOfMainAssets.output)) 
+                  received=moveOfMainAssets.received, output=moveOfMainAssets.output, data=moveOfMainAssets.data)) 
 for data in move_array:
     printMoveOfMainAssets(data)
 
 def printTypeOfMainAssets(typeOfMainAssets):
     '''printTypeOfMainAssets function prints
-    "Вид основних засобів"
+    "Довідник ринків"
     with names and values'''
 
-    print("Код: {code}, Тип: {code}"
-        .format(code=typeOfMainAssets.code, type=typeOfMainAssets.type))
+    print("Код: {code}, Найменування ринку: {name}"
+        .format(code=typeOfMainAssets.code, name=typeOfMainAssets.name))
 
 for data in type_array:
     printTypeOfMainAssets(data)
